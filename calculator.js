04cf7d612d818bb80c9b7b22cfd080489e6e2455
Ekaterina Schedlberger <ek.schedl@gmail.com>
2024-09-17 14:33:43 +0200

    calculator is ready

--- calculator.js
+++ calculator.js
@@ -1,9 +1,3 @@
-let num = 41;
-let firstName = "Katja";
-
-num = 80;
-const isProgrammer = true;
-
 /* group of comments*/
 
 const resultElement = document.getElementById("result");
@@ -14,6 +8,8 @@ const input2 = document.getElementById("input2");
 const submitBtn = document.getElementById("submit");
 const plusBtn = document.getElementById("plus");
 const minusBtn = document.getElementById("minus");
+const multiplicationBtn = document.getElementById("multiplication");
+const divisionBtn = document.getElementById("division");
 let action = "+";
 
 // console.log(typeof sum);
@@ -26,13 +22,37 @@ minusBtn.onclick = function () {
   action = "-";
 };
 
+multiplicationBtn.onclick = function () {
+  action = "*";
+};
+
+divisionBtn.onclick = function () {
+  action = "/";
+};
+
+function printResult(result) {
+  if (result < 0) {
+    resultElement.style.color = "red";
+  } else {
+    resultElement.style.color = "green";
+  }
+  resultElement.textContent = result;
+}
+
+function computeNumbersWithAction(inp1, inp2, actionSymbol) {
+  const num1 = Number(inp1.value);
+  const num2 = Number(inp2.value);
+  if (actionSymbol == "+") {
+    return num1 + num2;
+  } else if (actionSymbol == "-") {
+    return num1 - num2;
+  } else if (actionSymbol == "*") {
+    return num1 * num2;
+  } else if (actionSymbol == "/") {
+    return num1 / num2;
+  }
+}
 submitBtn.onclick = function () {
-  if (action == "+") {
-    const sum = Number(input1.value) + Number(input2.value);
-    resultElement.textContent = sum;
-  }
-  if (action == "-") {
-    const sum = Number(input1.value) - Number(input2.value);
-    resultElement.textContent = sum;
-  }
+  const result = computeNumbersWithAction(input1, input2, action);
+  printResult(result);
 };
